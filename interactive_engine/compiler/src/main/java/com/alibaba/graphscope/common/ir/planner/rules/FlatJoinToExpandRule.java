8b5717fa8d968d5cf01b7cb05d6a4cd55d0e9e44
shirly121 <yihe.zxl@alibaba-inc.com>
2024-11-06 19:31:36 +0800

    add FlatJoinToCommonRule to optimize bi14

--- interactive_engine/compiler/src/main/java/com/alibaba/graphscope/common/ir/planner/rules/FlatJoinToExpandRule.java
+++ interactive_engine/compiler/src/main/java/com/alibaba/graphscope/common/ir/planner/rules/FlatJoinToExpandRule.java
@@ -17,8 +17,6 @@
  */
 package com.alibaba.graphscope.common.ir.planner.rules;
 
-import com.alibaba.graphscope.common.config.PlannerConfig;
-import com.alibaba.graphscope.common.ir.rel.GraphShuttle;
 import com.alibaba.graphscope.common.ir.rel.graph.GraphLogicalSource;
 import com.alibaba.graphscope.common.ir.rel.graph.match.GraphLogicalSingleMatch;
 import com.alibaba.graphscope.common.ir.rel.type.AliasNameWithId;
@@ -27,27 +25,13 @@ import com.google.common.collect.Lists;
 
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.core.JoinRelType;
-import org.apache.calcite.rel.logical.LogicalFilter;
 import org.apache.calcite.rel.logical.LogicalJoin;
 import org.apache.calcite.rex.RexNode;
-import org.apache.calcite.rex.RexUtil;
 
 import java.util.List;
 
 public class FlatJoinToExpandRule extends FlatJoinRule {
-    // left plan of the join
-    private RelNode left;
-    // alias of the left key in the join condition
-    private AliasNameWithId leftAlias;
-    // whether the 'MatchOpt' in the right plan is optional
-    private boolean optional;
-    private List<RexNode> otherJoinConditions;
-
-    private final PlannerConfig config;
-
-    public FlatJoinToExpandRule(PlannerConfig config) {
-        this.config = config;
-    }
+    private ExpandFlatter expandFlatter;
 
     @Override
     protected boolean matches(LogicalJoin join) {
@@ -67,48 +51,18 @@ public class FlatJoinToExpandRule extends FlatJoinRule {
         // check whether the sentence starts or ends with the alias id in the join condition
         boolean contains = startEndAliasIds.contains(var.getAliasId());
         if (contains) {
-            left = join.getLeft();
-            leftAlias = new AliasNameWithId(var.getName().split("\\.")[0], var.getAliasId());
-            optional = join.getJoinType() == JoinRelType.LEFT;
-            otherJoinConditions = others;
+            this.expandFlatter =
+                    new ExpandFlatter(
+                            join.getLeft(),
+                            new AliasNameWithId(var.getName().split("\\.")[0], var.getAliasId()),
+                            join.getJoinType() == JoinRelType.LEFT,
+                            others);
         }
         return contains;
     }
 
     @Override
     protected RelNode perform(LogicalJoin join) {
-        return join.getRight().accept(new ExpandFlatter());
-    }
-
-    // perform the transformation from the join plan to the expand plan
-    // replace the source operator of the right plan with the left plan,
-    // i.e. join(getV1->expand1->source1, getV2->expand2->source2) =>
-    // getV2->expand2->getV1->expand1->source1
-    private class ExpandFlatter extends GraphShuttle {
-        @Override
-        public RelNode visit(GraphLogicalSingleMatch match) {
-            RelNode sentence = match.getSentence();
-            // reverse the sentence if the expand order does not start from the 'leftAlias'
-            RelNode source = getSource(sentence);
-            if (getAliasId(source) != leftAlias.getAliasId()) {
-                sentence = sentence.accept(new Reverse(sentence));
-            }
-            // set expand operators in the right plan as optional if 'optional' = true
-            if (optional) {
-                sentence = sentence.accept(new SetOptional());
-            }
-            // replace the source operator of the sentence with the left plan
-            RelNode expand = sentence.accept(new ReplaceInput(leftAlias, left));
-            // if there are other join conditions, add a filter operator on top of the expand
-            // operator
-            if (!otherJoinConditions.isEmpty()) {
-                RexNode otherJoinCondition =
-                        RexUtil.composeConjunction(
-                                match.getCluster().getRexBuilder(), otherJoinConditions);
-                LogicalFilter filter = LogicalFilter.create(expand, otherJoinCondition);
-                expand = filter;
-            }
-            return expand;
-        }
+        return join.getRight().accept(this.expandFlatter);
     }
 }
